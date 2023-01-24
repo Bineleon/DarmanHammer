93d62ec90c3c44e5a86eab1f0dca5e8862f027db
Nelbi Leon <nelbi.leon@gmail.com>
2023-01-24 23:33:46 +0100

    First commit

---
+++ darmanhammer.rb
@@ -0,0 +1,21 @@
+require 'gosu'
+
+class WhackARuby < Gosu::Window
+
+  def initialize
+    super 800, 600, false
+  end
+
+  def draw
+  end
+
+  def update
+  end
+
+  def button_down(id)
+  end
+
+end
+
+window = WhackARuby.new
+window.show
